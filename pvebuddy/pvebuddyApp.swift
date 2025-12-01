7095f7654b8f07dd91b3344f36d861caaf466c89
Oliver Steiner <github@jsemolik.dev>
2025-12-01 11:59:23 +0100

    Initial Commit

---
+++ pvebuddy/pvebuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pvebuddyApp.swift
+//  pvebuddy
+//
+//  Created by Oliver Steiner on 01.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct pvebuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
